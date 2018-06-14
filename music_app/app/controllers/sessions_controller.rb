44761e3a0e1ac083574922ef4f49ed74a40781e7
Alex <a.plyushchay@gmail.com>
2018-06-14 10:39:29 -0400

    Finish Controllers

--- music_app/app/controllers/sessions_controller.rb
+++ music_app/app/controllers/sessions_controller.rb
@@ -1,3 +1,28 @@
 class SessionsController < ApplicationController
-
+  def new
+    user = User.new
+    render :new
+  end
+  
+  def create
+    email = user_params[:email]
+    password = user_params[:password]
+    user = User.find_by_credentials(email, password)
+    
+    if user
+      login!(user)
+      redirect_to user_url
+    else
+      render :new
+    end
+  end
+  
+  def destroy
+    logout!
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
