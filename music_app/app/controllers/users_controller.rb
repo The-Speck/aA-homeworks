99390aae9536e0c855ec1f15548cd8f4d512bb9b
Alex <a.plyushchay@gmail.com>
2018-06-14 10:21:59 -0400

    Finish UsersController

--- music_app/app/controllers/users_controller.rb
+++ music_app/app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
-
+  
+  def new
+    @user = User.new
+    render :new
+  end
+  
+  def create
+    user = User.create(user_params)
+    
+    if user.save
+      session[:session_token] = user.session_token
+      redirect_to :new
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
