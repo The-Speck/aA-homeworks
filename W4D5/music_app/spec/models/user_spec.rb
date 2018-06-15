ea4ba02d19e107eb6434dfab416df7ac1ef9f5fe
Alex <A.plyushchay@gmail.com>
2018-06-15 09:01:20 -0400

    Controller Spec

--- W4D5/music_app/spec/models/user_spec.rb
+++ W4D5/music_app/spec/models/user_spec.rb
@@ -9,17 +9,23 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:password_digest) }
   it { should validate_length_of(:password).is_at_least(6) }
 
-
-  it 'should test password equals password digest' do
-    expect(user.is_password?(user.password)).to be true
+  describe "#is_password?" do
+    it 'should test password equals password digest' do
+      expect(user.is_password?(user.password)).to be true
+    end
   end
 
-  it 'should assign a new session token' do
-    token = user.reset_session_token!.dup
-    expect(user.reset_session_token!).to_not eq(token)
+  describe "#reset_session_token" do
+    it 'should assign a new session token' do
+      token = user.reset_session_token!.dup
+      expect(user.reset_session_token!).to_not eq(token)
+    end
   end
 
-  it 'should find by credentials' do
-    expect(User.find_by_credentials(user.email, user.password)).to eql(user)
+  describe "::find_by_credentials" do
+    before { user.save! }
+    it 'should find by credentials' do
+      expect(User.find_by_credentials(user.email, user.password)).to eq(user)
+    end
   end
 end
